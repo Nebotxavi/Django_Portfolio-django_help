804af5da14ac28346bbfcddde9ddcc2b8fa56edf
balaclava <nebotxavi@gmail.com>
2019-10-04 09:53:07 +0200

    Debug set to false

--- django_help/settings.py
+++ django_help/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUT_VALUE')) == 'True'
 
 ALLOWED_HOSTS = ['django-help.herokuapp.com']
 
