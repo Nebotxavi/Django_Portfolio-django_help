62306733854111a4410073abb133e0d4d3b25379
balaclava <nebotxavi@gmail.com>
2019-10-24 08:46:12 +0200

    CRUD testing

--- blog/tests.py
+++ blog/tests.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
+from .models import Post
+
 
 class HomeAndAbout(TestCase):
     def test_home_status_code(self):
@@ -123,3 +125,160 @@ class SignUp(TestCase):
                          [0].username, "testing_user")
         self.assertEqual(get_user_model().objects.all()
                          [0].email, "testing@mail.com")
+
+
+class CRUD(TestCase):
+    def setUp(self):
+        self.test_user = get_user_model().objects.create_user(
+            username="testing_user",
+            email="testing@mail.com",
+            password="testing_password"
+        )
+
+        login = self.client.login(
+            username='testing_user',
+            password='testing_password'
+        )
+
+        self.test_post = Post.objects.create(
+            title="testing title",
+            content="testing content",
+            author=self.test_user
+        )
+
+    # Read
+    def test_detail_content(self):
+        resp = self.client.get(reverse('post_detail', args='1'))
+        self.assertContains(resp, 'testing title')
+        self.assertContains(resp, 'testing content')
+        self.assertContains(resp, 'testing_user')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_detail_post_status_code(self):
+        resp = self.client.get('/post/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_detail_post_template(self):
+        resp = self.client.get(reverse('post_detail', args='1'))
+        self.assertTemplateUsed(resp, 'blog/post_detail.html')
+
+    # Create
+
+    def test_new_post_status_code(self):
+        resp = self.client.get('/post/new_post/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_new_post_name_status_code(self):
+        resp = self.client.get(reverse('post_create'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_new_post_template_status_code(self):
+        resp = self.client.get('/post/new_post/')
+        self.assertTemplateUsed(resp, 'blog/post_form.html')
+
+    def test_new_post_create(self):
+        resp = self.client.post('/post/new_post/', {
+            'title': 'testing title 2',
+            'content': 'testing content 2',
+        })
+
+        self.assertEqual(resp.status_code, 302)
+
+        resp = self.client.get(reverse('post_detail', args='2'))
+        self.assertContains(resp, 'testing title 2')
+        self.assertContains(resp, 'testing content 2')
+        self.assertContains(resp, 'testing_user')
+        self.assertNotContains(resp, 'testing title 1')
+
+    # Update
+
+    def test_update_post_status_code(self):
+        resp = self.client.get('/post/1/update/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_update_post_name_status_code(self):
+        resp = self.client.get(reverse('post_update', args='1'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_update_post_template(self):
+        resp = self.client.get('/post/1/update/')
+        self.assertTemplateUsed(resp, 'blog/post_form.html')
+
+    def test_update_post_update(self):
+        resp = self.client.post('/post/1/update/', {
+            'title': 'testing title updated',
+            'content': 'testing content updated',
+            'tags': 'new testing tag'
+        })
+
+        self.assertEqual(resp.status_code, 302)
+
+        resp = self.client.get(reverse('post_detail', args='1'))
+        self.assertContains(resp, 'testing title updated')
+        self.assertContains(resp, 'testing content updated')
+        self.assertContains(resp, 'testing_user')
+        self.assertNotContains(resp, 'testing title 1')
+
+    # Remove
+
+    def test_delete_post_status_code(self):
+        resp = self.client.get('/post/1/delete/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_delete_post_name_status_code(self):
+        resp = self.client.get(reverse('post_delete', args='1'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_delete_post_template(self):
+        resp = self.client.get('/post/1/delete/')
+        self.assertTemplateUsed(resp, 'blog/post_confirm_delete.html')
+
+    def test_delete_post_delete(self):
+        resp = self.client.post('/post/1/delete/')
+        self.assertEqual(Post.objects.all().count(), 0)
+
+
+class Tags(TestCase):
+    def setUp(self):
+        self.test_user = get_user_model().objects.create_user(
+            username="testing_user",
+            email="testing@mail.com",
+            password="testing_password"
+        )
+
+        login = self.client.login(
+            username='testing_user',
+            password='testing_password'
+        )
+
+        self.test_post = Post.objects.create(
+            title="testing title",
+            content="testing content",
+            tags="testing_tag",
+            author=self.test_user
+        )
+
+    def test_tags(self):
+        self.assertEqual(Post.tags.all().count(), 0)
+       # self.assertContain(Post.tags.get(name='testing_tag'), 'testing_tag')
+       # TAGS cannot introduced just by writing tags='text'
+
+
+class Search(TestCase):
+    def setUp(self):
+        self.test_user = get_user_model().objects.create_user(
+            username="testing_user",
+            email="testing@mail.com",
+            password="testing_password"
+        )
+
+        login = self.client.login(
+            username='testing_user',
+            password='testing_password'
+        )
+
+        self.test_post = Post.objects.create(
+            title="testing title",
+            content="testing content",
+            author=self.test_user
+        )
