e57a07e83bfd835be820476b0333d82ab615afbc
balaclava <nebotxavi@gmail.com>
2019-11-26 16:28:37 +0100

    API CRUD and CORS in settings

---
+++ blog/API/urls.py
@@ -0,0 +1,16 @@
+from .views import (PostListAPIView,
+                    PostDetailAPIView,
+                    PostUpdateAPIView,
+                    PostDeleteAPIView,
+                    PostCreateAPIView)
+
+from django.urls import path
+
+
+urlpatterns = [
+    path('', PostListAPIView.as_view(), name='list'),
+    path('<int:pk>/', PostDetailAPIView.as_view(), name='detail'),
+    path('<int:pk>/edit/', PostUpdateAPIView.as_view(), name='update'),
+    path('<int:pk>/delete/', PostDeleteAPIView.as_view(), name='delete'),
+    path('create/', PostCreateAPIView.as_view(), name='create')
+]
--- django_help/settings.py
+++ django_help/settings.py
@@ -50,11 +50,16 @@ INSTALLED_APPS = [
     'django_cleanup',
     'storages',
     'taggit',
+    'corsheaders',
+
+    # api
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,21 +140,6 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'login'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("USER_MAIL")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
-
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -161,8 +151,41 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("USER_MAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+
+# CORS CONFIG
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = ['http://localhost:8000', ]
+CORS_ORIGIN_REGEX_WHITELIST = ['https://localhost:8000']
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 TAGGIT_CASE_INSENSITIVE = True
 
 django_heroku.settings(locals())
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        # 'rest_framework.permissions.IsAuthenticated'
+    ]
+}
